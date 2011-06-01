00038fc755086293f6d74d9e574563f90afc78a5
Sean McDaniel <sean.mcdaniel@me.com>
2011-06-01 19:49:38 -0400

    rework

--- blue_hawaii/reservation_service.rb
+++ blue_hawaii/reservation_service.rb
@@ -1,11 +1,23 @@
 require 'date'
 require 'json'
 
-module ReservationFinder
+module ReservationService
   SALES_TAX = 4.11416
   
+  #
+  # Run the reservation request. 
+  #
   def self.run
-    inventory = open('resources/vacation_rentals.json') do |f| 
+    lister = ReservationLister.new(load_inventory)
+    listing = File.open('reservation_listing.txt', 'w+')
+    listing.puts  lister.list(load_period)  
+  end
+  
+  #
+  # Loads the property inventory.
+  #
+  def self.load_inventory
+    open('vacation_rentals.json') do |f| 
       JSON.parse(f.gets).collect do |unit|
         if unit['seasons']
           seasons = unit['seasons'].collect do |season|
@@ -13,27 +25,32 @@ module ReservationFinder
             Season.new(season['start'], season['end'], season['rate'])    
           end
         end
-
+        
         Property.new(unit['name'], unit['rate'], seasons, unit['cleaing fee'])      
       end
     end
-    
-    # load the vacation period
-    period = open('resources/input.txt') do |f|  
+  end
+  
+  #
+  # Loads the reservation period.
+  #
+  def self.load_period
+    open('input.txt') do |f|  
       dates = f.gets.split('-')
       Date.parse(dates.first)..Date.parse(dates.last)
     end
-
-    calculator = VacationCalculator.new(inventory)
-    puts calculator.list(period)    
   end
 
   #
-  # Internal represenation of a rental seasion.
+  # Internal represenation of a rental season (range like).
   #
   class Season
     def initialize(from, to, rate)
       @from, @to, @rate = from, to, rate
+    end
+    
+    def contains(date)
+        
     end
   end    
 
@@ -45,12 +62,16 @@ module ReservationFinder
     def initialize(name, rate, seasons, cleaning_fee)
       @name, @rate, @seasons, @cleaning_fee = name, rate, seasons, cleaning_fee      
     end
+    
+    def calculate(period)
+      puts period.end - period.start
+    end
   end
     
   #
   # Finds available properties in the inventory for a given period.
   #
-  class VacationCalculator
+  class ReservationLister
     def initialize(inventory)
       @inventory = inventory
     end
@@ -63,4 +84,4 @@ module ReservationFinder
   end
 end
 
-ReservationFinder.run
+ReservationService.run
