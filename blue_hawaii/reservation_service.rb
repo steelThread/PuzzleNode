3bb13bdc89259dd71536c38427c98da54f55e539
Sean McDaniel <sean.mcdaniel@me.com>
2011-06-01 22:56:31 -0400

    refactor

--- blue_hawaii/reservation_service.rb
+++ blue_hawaii/reservation_service.rb
@@ -1,13 +1,14 @@
+require 'bigdecimal'
 require 'date'
 require 'json'
 
 module ReservationService
-  SALES_TAX = 4.11416
+  SALES_TAX = BigDecimal.new('.0411416')
   
   #
-  # Run the reservation request. 
+  # List the price of the inventory for a given period. 
   #
-  def self.run
+  def self.list
     listing   = File.open('reservation_listing.txt', 'w+')
     period    = load_period
     inventory = load_inventory
@@ -25,11 +26,13 @@ module ReservationService
         if unit['seasons']
           seasons = unit['seasons'].collect do |season|
             season = season.values[0]
-            Season.new(season['start'], season['end'], season['rate'])    
+            rate   = BigDecimal.new(season['rate'][1..-1])
+            Season.new(season['start'], season['end'], rate)    
           end
         end
         
-        Property.new(unit['name'], unit['rate'], seasons, unit['cleaing fee'])      
+        rate = BigDecimal.new(unit['rate'][1..-1]) if unit['rate']
+        Property.new(unit['name'], rate, seasons, unit['cleaing fee'])      
       end
     end
   end
@@ -40,12 +43,22 @@ module ReservationService
   def self.load_period
     open('input.txt') do |f|  
       dates = f.gets.split('-')
-      Date.parse(dates.first)..Date.parse(dates.last)
+      Period.new(Date.parse(dates.first), Date.parse(dates.last))
+    end
+  end
+
+  class Period
+    def initialize(from, to)
+      @from, @to = from, to
+    end
+    
+    def days
+      (@to - @from).to_i
     end
   end
 
   #
-  # Internal represenation of a rental season (range like).
+  # Internal representation of a rental season (range like).
   #
   class Season
     def initialize(from, to, rate)
@@ -62,10 +75,21 @@ module ReservationService
       @name, @rate, @seasons, @cleaning_fee = name, rate, seasons, cleaning_fee      
     end
     
+    def seasonal?
+      !@seasons.nil?
+    end
+    
     def price(period)
-      '$0.00'
+      if seasonal?
+        '$?.??'
+      else
+        price  = period.days * @rate
+        price += price * SALES_TAX 
+        price += @cleaning_fee if @cleaning_fee
+        sprintf("$%.02f", price)
+      end
     end
   end
 end
 
-ReservationService.run
+ReservationService.list
